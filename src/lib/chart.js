28be0ff9ac9bbb478870542dc1657a26b2c01b5c
Jon McClure <jon.r.mcclure@gmail.com>
2020-07-30 21:47:29 +0100

    fixes appends. Adds spike color scale as prop. Different mouseover behavior.

--- src/lib/chart.js
+++ src/lib/chart.js
@@ -1,14 +1,15 @@
-import ChartComponent from './base/ChartComponent';
-import d3 from './utils/d3';
 import * as topojson from 'topojson-client';
 
-import AtlasMetadataClient from '@reuters-graphics/graphics-atlas-client';
-const client = new AtlasMetadataClient();
+// import AtlasMetadataClient from '@reuters-graphics/graphics-atlas-client';
+import ChartComponent from './base/ChartComponent';
+import d3 from './utils/d3';
+
+// const client = new AtlasMetadataClient();
 // import topology from '@reuters-graphics/graphics-atlas-client/topojson/global.110m.json';
 
 class GlobalRateMap extends ChartComponent {
   defaultProps = {
-    map_stroke_width: .7,
+    map_stroke_width: 0.7,
     map_stroke_color: 'rgba(255, 255, 255, 0.15)',
     map_stroke_color_active: 'rgba(255, 255, 255, 0.5)',
     spike_color: '#eec331',
@@ -18,9 +19,12 @@ class GlobalRateMap extends ChartComponent {
     hover_gap: 12.5,
     spike_height: 20,
     spike_size: 2,
-    range: { min: 0, max: 1 },
+    range: { min: 0.75, max: 1 },
     spike_stroke_width: 0.8,
-    spike_highlight_stroke_width: 1.2
+    spike_highlight_stroke_width: 1.2,
+    spike_color_scale: d3.scaleThreshold() // Can use a scale as a prop!
+      .domain([0.75, 0.9])
+      .range(['#ccc', '#f68e26', '#de2d26']),
   };
 
   draw() {
@@ -29,9 +33,9 @@ class GlobalRateMap extends ChartComponent {
     const node = this.selection().node();
     const { width } = node.getBoundingClientRect();
 
-    const newData = data.filter(d => d.value >= props.range.min && d.value <= props.range.max)
+    const newData = data.filter(d => d.value >= props.range.min && d.value <= props.range.max);
 
-    const scaleY = d3.scaleLinear().range([0, props.spike_height]).domain([0, 1])
+    const scaleY = d3.scaleLinear().range([0, props.spike_height]).domain([0, 1]);
     const svg = this.selection()
       .appendSelect('svg') // see docs in ./utils/d3.js
       .attr('width', width)
@@ -50,62 +54,67 @@ class GlobalRateMap extends ChartComponent {
     g.selectAll('.country').remove();
 
     const countryGroups = g.selectAll('g.country')
-      .data(countries.features.filter(d => d.properties.slug != 'antarctica'))
+      .data(countries.features.filter(d => d.properties.slug !== 'antarctica'))
       .enter()
       .append('g')
       .attr('class', 'country');
 
-    countryGroups.append('path')
+    countryGroups.appendSelect('path.level-0')
       .attr('class', d => 'c-' + d.properties.slug + ' level-0')
       .style('stroke', props.map_stroke_color)
       .style('stroke-width', props.map_stroke_width)
-      .style('fill', 'rgba(255,255,255,0)')
+      .style('fill', 'transparent')
       .attr('d', path);
 
     countryGroups
-      .append('path')
+      .appendSelect('path.centroid')
       .attr('class', d => d.properties.slug + ' centroid')
       .attr('d', function(d) {
         const obj = projection(d.properties.centroid);
         const o = newData.filter(e => d.properties.isoAlpha2 === e.key)[0];
         if (o) {
           const value = scaleY(o.value);
-          return 'M' + (obj[0] - props.spike_size) + ' ' + obj[1] + ' L' + obj[0] + ' ' + (obj[1] - value)+ ' L' + (obj[0] + props.spike_size) + ' ' + obj[1] + ' ';
+          return 'M' + (obj[0] - props.spike_size) + ' ' + obj[1] + ' L' + obj[0] + ' ' + (obj[1] - value) + ' L' + (obj[0] + props.spike_size) + ' ' + obj[1] + ' ';
         }
       })
       .style('fill', 'none')
       .style('stroke', function(d) {
         const o = newData.filter(e => d.properties.isoAlpha2 === e.key)[0];
-        if (o) {
-          if (o.value >= 0.9) {
-            return '#de2d26';
-          } else if (o.value >= 0.75) {
-            return '#f68e26';
-          } else {
-            return 'rgba(255,255,255,0.8)';
-          }
-        }
+        return o ? props.spike_color_scale(o.value) : null;
       })
       .style('stroke-width', props.spike_stroke_width);
 
-    g.append('path').attr('class', 'disputed level-0')
+    g.appendSelect('path.disputed')
+      .attr('class', 'disputed level-0')
       .style('stroke', props.map_stroke_color)
       .style('stroke-width', props.map_stroke_width)
       .attr('d', path(disputed));
 
-    countryGroups.on('mouseover', tipOn)
+    countryGroups
+      .style('pointer-events', 'none')
+      .style('pointer-events', function(d) {
+        const o = newData.filter(e => d.properties.isoAlpha2 === e.key)[0];
+        if (!o) return 'none';
+        if (
+          o.value >= props.range.min &&
+          o.value <= props.range.max
+        ) return 'auto';
+        return 'none';
+      })
+      .on('mouseover', tipOn)
       .on('mouseout', tipOff);
 
     function tipOn(obj) {
       const sel = d3.select(this);
-      g.selectAll('.country').style('opacity', 0.2);
+      g.selectAll('path.centroid').style('opacity', 0);
+      g.selectAll(`path.centroid.${obj.properties.slug}`).style('opacity', 1);
       sel.appendSelect('text')
-        .attr('transform',function(d) {
-          const o = projection(d.properties.centroid)
-          return `translate(${o[0]},${o[1] + props.hover_gap})`
+        .attr('transform', function(d) {
+          const o = projection(d.properties.centroid);
+          return `translate(${o[0]},${o[1] + props.hover_gap})`;
         })
-        .style('text-anchor','middle')
-        .text(d=>d.properties.translations[props.locale]);
+        .style('text-anchor', 'middle')
+        .text(d => d.properties.translations[props.locale]);
 
       sel.style('opacity', 1);
       sel.selectAll('.level-0')
@@ -118,9 +127,9 @@ class GlobalRateMap extends ChartComponent {
     }
 
     function tipOff(obj) {
-      g.selectAll('.country').style('opacity', 1);
+      g.selectAll('path.centroid').style('opacity', 1);
       const sel = d3.select(this);
-      sel.select('text').remove()
+      sel.select('text').remove();
       sel.selectAll('.level-0')
         .classed('active', false)
         .style('stroke', props.map_stroke_color);
